0ef5d1bd4eb46704369600d9db82155dce130c6f
lyhoanam <lyhoanam@gmail.com>
2025-01-02 16:29:06 +0700

    chore: change title

--- src/components/atom/Header.tsx
+++ src/components/atom/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header className="text-6xl text-orange-700 font-bold mb-2 text-center">
-      What's pokemon
+      What type of pokemon?
     </header>
   );
 };
