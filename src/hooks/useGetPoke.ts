ff462e11f79ab1ada70d602d8233221b495e639c
lyhoanam <lyhoanam@gmail.com>
2025-01-02 16:23:59 +0700

    feat: shuffle initial data

--- src/hooks/useGetPoke.ts
+++ src/hooks/useGetPoke.ts
@@ -4,6 +4,8 @@ import { LIMIT, OFFSET } from "@src/constants/BASE";
 import { getApi } from "@src/utils/api";
 
 import { useDispatch } from "./core";
+import { shuffle } from "@src/utils";
+import type { PokeCard } from "@src/stores/pokeType";
 
 export const useGetApi = () => {
   const dispatch = useDispatch();
@@ -11,9 +13,9 @@ export const useGetApi = () => {
     (async () => {
       const data = await getApi(`/?limit=${LIMIT}&offset=${OFFSET}`);
       if (data.results && data.results.length > 0) {
-        const pokes = data.results.filter(
+        const pokes = shuffle<PokeCard>(data.results.filter(
           (record: { name: string }) => record.name
-        );
+        ));
         dispatch({ type: "INITIAL_POKE", payload: pokes });
       }
     })();
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,19 @@ import { IMG_TYPE, IMG_URL } from "@src/constants/BASE";
 export const getUrlImg = (name: string) => {
   return `${IMG_URL}/${name}.${IMG_TYPE}`;
 };
+
+export const shuffle = <T>(array: T[]): T[] => {
+  let currentIndex = array.length;
+
+  while (currentIndex !== 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
